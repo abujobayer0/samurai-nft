e48bd3c39e98805e08b6626f9b13b40beab4e000
zubayermunna <muna700064@gmail.com>
2023-03-06 00:20:53 +0600

    added sponser

--- src/components/featureSection/index.js
+++ src/components/featureSection/index.js
@@ -9,8 +9,21 @@ import {
 const FeaturedSection = () => {
   return (
     <>
-      <section className="max-w-7xl mt-32 mx-auto px-10">
-        <h1 className="clash-700 text-[38px]">Featured & Partners</h1>
+      <section className="max-w-7xl mt-32 mx-auto px-5 sm:px-10">
+        <h1 className="clash-700 mb-10 md:mb-[79px] text-[28px] lg:text-[38px]">
+          Featured & Partners
+        </h1>
+        <div className="flex gap-10  md:gap-8  lg:gap-[61px] -rotate-2 flex-row items-center overflow-hidden justify-center  w-full">
+          <img
+            className="md:w-28  w-20 lg:w-44  "
+            src={yahooFinance}
+            alt="img"
+          />
+          <img className="md:w-32  w-20 lg:w-44  " src={googlePlay} alt="img" />
+          <img className="md:w-32  w-20 lg:w-44  " src={binance} alt="img" />
+          <img className="md:w-32  w-20 lg:w-44  " src={pinkSale} alt="img" />
+          <img className="md:w-32  w-20 lg:w-44  " src={dextools} alt="img" />
+        </div>
       </section>
     </>
   );

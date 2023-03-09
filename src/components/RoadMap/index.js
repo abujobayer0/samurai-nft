bbcceeb2c0cf23c677b6056b0eef077ce57d493f
zubayermunna <muna700064@gmail.com>
2023-03-09 12:48:13 +0600

    roadmap added

--- src/components/RoadMap/index.js
+++ src/components/RoadMap/index.js
@@ -45,7 +45,7 @@ const RoadMap = () => {
           className="w-full rotate-90 md:rotate-0 relative"
           alt="img"
         />
-        <div className="absolute top-0 lg:top-10 right-5 md:left-10">
+        <div className="absolute -top-20 md:top-0 lg:top-10 right-10 md:left-10">
           <TemplePoint no={"1"} TempleIcon={TempleIcon} />
         </div>
         <div className="absolute top-[20%] md:top-[30%] lg:top-[30%] right-[50%] md:left-[27%]">
@@ -57,34 +57,34 @@ const RoadMap = () => {
         <div className="absolute top-[100%] lg:top-[40%] lg:right-[5%] md:top-[50%] right-[70%] md:right-[0%]">
           <TemplePoint no={"4"} TempleIcon={TempleIcon} />
         </div>
-        <div className="absolute text-[#BDBDBD]  lg:text-[24px] lg:-top-10 right-5 md:left-[23%]">
+        <div className="absolute text-[9px] -top-8 right-[30%] md:text-[16px]  text-[#BDBDBD]  lg:text-[24px] lg:-top-10 md:left-5 md:top-44 lg:left-[23%]">
           {phrase1.map((item) => (
             <li>{item}</li>
           ))}
         </div>
-        <div className="absolute top-[20%] text-[24px] text-[#BDBDBD] md:top-[30%] lg:top-[40%] right-[50%] md:left-[50px]">
+        <div className="absolute top-[30%] text-[9px] left-5  md:text-[16px] lg:text-[24px] text-[#BDBDBD] md:left-[40%] md:top-[10%] lg:top-[40%]  lg:left-[50px]">
           {phrase2.map((item) => (
             <li>{item}</li>
           ))}
         </div>
-        <dir className="absolute w-[400px] top-[20%] text-[24px] text-[#BDBDBD] md:top-[30%] lg:top-[74%] right-[50%] md:left-[37%]">
+        <div className="absolute w-[400px] top-[44%] -right-32  text-[9px]  md:text-[16px] lg:text-[24px] text-[#BDBDBD] md:top-[74%] md:left-[30%] lg:top-[74%]  lg:left-[37%]">
           {phrase3.map((item) => (
-            <h1 className="flex w-[400px] justify-start items-center gap-4">
+            <h1 className="flex w-[400px] justify-start items-center gap-2 lg:gap-4">
               {" "}
-              <span className=" w-[10px] bg-[#BDBDBD] px-1 h-[10px] rounded-full "></span>
+              <span className=" md:w-[10px] w-[4px] bg-[#BDBDBD] md:px-1 h-[4px] md:h-[10px] rounded-full "></span>
               {item}
             </h1>
           ))}
-        </dir>
-        <dir className="absolute w-[400px] top-[20%] text-[24px] text-[#BDBDBD] md:top-[30%] lg:top-[20%] right-[50%] lg:left-[65%]">
+        </div>
+        <div className="absolute w-[400px]  bottom-0 left-5 text-[9px] md:text-[16px] lg:text-[24px] text-[#BDBDBD] md:top-[35%] md:left-[50%] lg:top-[20%]  lg:left-[65%]">
           {phrase4.map((item) => (
-            <h1 className="flex w-[400px] justify-start items-center gap-4">
+            <h1 className="flex w-[400px] justify-start items-center gap-2 md:gap-2 lg:gap-4">
               {" "}
-              <span className=" w-[10px] bg-[#BDBDBD] h-[10px] rounded-full "></span>{" "}
+              <span className=" w-[4px] md:w-[10px] bg-[#BDBDBD] h-[4px] md:h-[10px] rounded-full "></span>{" "}
               {item}
             </h1>
           ))}
-        </dir>
+        </div>
       </div>
     </div>
   );

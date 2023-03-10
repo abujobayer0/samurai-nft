7df14002960fbec5318b9bd265d2528cd11dbbd1
zubayermunna <muna700064@gmail.com>
2023-03-11 00:54:31 +0600

    added

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -24,37 +24,43 @@ export default function Faq() {
   return (
     <div className={"max-w-7xl px-5 lg:px-10  mx-auto mb-32"}>
       <Title size={"40px"} head={"Frequently Aske"} sub={"d Question"} />
+      <div className="-mt-10">
+        <Accordion transition transitionTimeout={200}>
+          <AccordionItem no={"001"} header="What is 0xSamurai?">
+            0xSamurai is a blockchain-based game that allows players to collect
+            and battle with NFTs in a virtual arena. Players can earn rare NFTs
+            and other rewards by winning battles and completing quests.
+          </AccordionItem>
 
-      <Accordion transition transitionTimeout={200}>
-        <AccordionItem no={"001"} header="What is 0xSamurai?">
-          0xSamurai is a blockchain-based game that allows players to collect
-          and battle with NFTs in a virtual arena. Players can earn rare NFTs
-          and other rewards by winning battles and completing quests.
-        </AccordionItem>
+          <AccordionItem no={"002"} header="Is 0xSamurai secure?">
+            Yes, 0xSamurai is built on the blockchain and utilizes smart
+            contracts to ensure that transactions are secure and transparent.
+            The platform has also undergone multiple security audits to ensure
+            the safety of user funds and data.
+          </AccordionItem>
 
-        <AccordionItem no={"002"} header="Is 0xSamurai secure?">
-          Yes, 0xSamurai is built on the blockchain and utilizes smart contracts
-          to ensure that transactions are secure and transparent. The platform
-          has also undergone multiple security audits to ensure the safety of
-          user funds and data.
-        </AccordionItem>
-
-        <AccordionItem no={"003"} header="What is RYO?">
-          There are several ways to earn rewards in 0xSamurai, including winning
-          battles, completing quests, staking RYO tokens, summoning Samurais.
-          Rewards can include rare NFTs, gold, and other valuable items.
-        </AccordionItem>
-        <AccordionItem no={"004"} header="What is RYO?">
-          Gold is an in-game currency that can be obtained by staking RYO
-          tokens. It can be used to purchase various items and upgrades within
-          the game.
-        </AccordionItem>
-        <AccordionItem no={"005"} header="How can I earn rewards in 0xSamurai?">
-          There are several ways to earn rewards in 0xSamurai, including winning
-          battles, completing quests, staking RYO tokens, summoning Samurais.
-          Rewards can include rare NFTs, gold, and other valuable items.
-        </AccordionItem>
-      </Accordion>
+          <AccordionItem no={"003"} header="What is RYO?">
+            There are several ways to earn rewards in 0xSamurai, including
+            winning battles, completing quests, staking RYO tokens, summoning
+            Samurais. Rewards can include rare NFTs, gold, and other valuable
+            items.
+          </AccordionItem>
+          <AccordionItem no={"004"} header="What is RYO?">
+            Gold is an in-game currency that can be obtained by staking RYO
+            tokens. It can be used to purchase various items and upgrades within
+            the game.
+          </AccordionItem>
+          <AccordionItem
+            no={"005"}
+            header="How can I earn rewards in 0xSamurai?"
+          >
+            There are several ways to earn rewards in 0xSamurai, including
+            winning battles, completing quests, staking RYO tokens, summoning
+            Samurais. Rewards can include rare NFTs, gold, and other valuable
+            items.
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }

9c199f76ba9c6567c2e8842fa15ea5fa941672c9
zubayermunna <muna700064@gmail.com>
2023-03-11 00:28:18 +0600

    added

--- src/components/coinTab/index.js
+++ src/components/coinTab/index.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { GoldCoin, RioCoin } from "../../assets";
 
 const CoinTab = ({ img, para }) => {
   return (
     <div
-      className="flex  gap-9 items-start justify-center mx-auto mb-10 md:mb-0  w-full h-fit flex-col 
+      className="flex  gap-9 items-center justify-center mx-auto mb-10 md:mb-0  w-full h-fit flex-col 
       lg:flex-row "
     >
-      <div className=" order-2 flex relative  mx-auto lg:justify-end justify-center items-center lg:items-end  lg:pl-8">
+      <div className=" order-2 flex relative w-full  mx-auto lg:justify-end justify-center items-center lg:items-end  lg:pl-8">
         <img
           loading="lazy"
           src={img}
-          className="w-full flex justify-center lg:justify-end items-center lg:items-end "
+          className={`${
+            img === GoldCoin ? "w-[350px]" : img === RioCoin ? "w-[558px]" : ""
+          } flex justify-center lg:justify-end items-center lg:items-end`}
           alt="coin"
         />
       </div>
       <div className="w-full flex items-center  ">
-        <p className="text-xs lg:w-[800px]  md:text-[16px] leading-6  fire-effect">
+        <p className="text-xs   md:text-[16px] leading-6  fire-effect">
           {para}
         </p>
       </div>
